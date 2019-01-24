4a2da1b32930ef1101fe476ba28d93a36253014a
ChenfuShi <40122452+ChenfuShi@users.noreply.github.com>
2019-01-24 11:40:14 +0000

    a

--- scripts/interaction_to_sparse.py
+++ scripts/interaction_to_sparse.py
@@ -34,12 +34,13 @@ def HiCpro_to_sparse(folder,resfrag,sizes,temporary_loc):
     file_valid_pairs, file_self_circle, file_dangling, file_religation = Prepare_files(folder,temporary_loc)
 
     distribution_nice_fragments = test_distancefromsite(file_valid_pairs,chroms_offsets,valid_chroms,frag_prop,frag_index)
+    #smooth the distribution out so it can be used directly in the read assignment
     # make the sparse matrix. sends a file at a time and adds stuff to the matrix
     coo_data = []
     coo_row = []
     coo_col = []
     for current_file in [file_valid_pairs, file_self_circle, file_religation]:
-        coo_data, coo_row, coo_col = Update_coo_lists_site(current_file,coo_data, coo_row, coo_col,chroms_offsets,valid_chroms,frag_prop,frag_index)
+        coo_data, coo_row, coo_col = Update_coo_lists_site(current_file,coo_data, coo_row, coo_col,chroms_offsets,valid_chroms,frag_prop,frag_index,distribution_nice_fragments)
     coo_data, coo_row, coo_col = Update_coo_lists_site(file_dangling,coo_data, coo_row, coo_col,chroms_offsets,valid_chroms,frag_prop,frag_index,dangling = True)
     
     CSR_mat = scipy.sparse.csr_matrix((coo_data, (coo_row, coo_col)), shape=(len(frag_index)+1, len(frag_index)+1), dtype = numpy.float32)
@@ -83,15 +84,15 @@ def Prepare_files(folder,temporary_loc):
     #############################################################################################################################################
     #############################################################################################################################################
     #############################################################################################################################################
-    shcommands = []
-    for files , output in zip((list_self_circle, list_dangling, list_religation),(file_self_circle, file_dangling, file_religation)):
-        command ="sort -u -k 2,2 -k 3,3 -k 5,5 -k 6,6 " + " ".join(files) + " > " + output
-        p = subprocess.Popen(command, shell=True)
-        # runs commands in parallel
-        shcommands.append(p)
-    for p in shcommands:
-        #waits for them to finish
-        p.wait()
+    # shcommands = []
+    # for files , output in zip((list_self_circle, list_dangling, list_religation),(file_self_circle, file_dangling, file_religation)):
+    #     command ="sort -u -k 2,2 -k 3,3 -k 5,5 -k 6,6 " + " ".join(files) + " > " + output
+    #     p = subprocess.Popen(command, shell=True)
+    #     # runs commands in parallel
+    #     shcommands.append(p)
+    # for p in shcommands:
+    #     #waits for them to finish
+    #     p.wait()
     
 
     return file_valid_pairs, file_self_circle, file_dangling, file_religation
@@ -158,7 +159,7 @@ def Update_coo_lists(current_file,data,row,col,offsets,valid_chroms):
 
     return data, row, col
 
-def Update_coo_lists_site(current_file,data, row, col,offsets,valid_chroms,frag_prop,frag_index,dangling = False,reassign = True):
+def Update_coo_lists_site(current_file,data, row, col,offsets,valid_chroms,frag_prop,frag_index,distribution_nice_fragments=None,dangling = False,reassign = True):
     """Takes file and assigns reads to restriction sites"""
     with open(current_file, "r") as pairs:
         for line in pairs:
@@ -175,7 +176,8 @@ def Update_coo_lists_site(current_file,data, row, col,offsets,valid_chroms,frag_
                 index_frag_1 = frag_index[frag_1]
                 index_frag_2 = frag_index[frag_2]
                 if reassign and not dangling:
-                    #check that fragment is correct
+                    #check that fragment is correct. will assign partial values properly depending on distance and distribution
+                    # reassign read by 5' positioning
                     if dir_1 == "+":
                         if pos_1 < frag_prop[index_frag_1][1]:
                             index_frag_1 -= 1
@@ -188,65 +190,187 @@ def Update_coo_lists_site(current_file,data, row, col,offsets,valid_chroms,frag_
                     else:
                         if pos_2 > frag_prop[index_frag_2][2]:
                             index_frag_2 += 1
-                if dangling:
-                    if dir_1 == "-":
-                        index_frag_1 += 1
-                    if dir_2 == "-":
-                        index_frag_2 += 1
-                else:
-                    if dir_1 == "+":
-                        index_frag_1 += 1
-                    if dir_2 == "+":
-                        index_frag_2 += 1
+                    #weigth what is the probability for the nearest fragment and the next fragment
+                    #in general if the reassign happened is because the next fragment would have been very small?? it should surely go back in the next phase??
+                    try:
+                        if dir_1 == "+":
+                            if frag_prop[index_frag_1+1][2] - pos_1 < 500:
+                                dist_1a = frag_prop[index_frag_1+1][1] - pos_1
+                                dist_1b = frag_prop[index_frag_1+1][2] - pos_1
+                                weight_1a = distribution_nice_fragments[dist_1a]
+                                weight_1b = distribution_nice_fragments[dist_1b]
+                            else:
+                                weight_1a = 1
+                                weight_1b = 0
+                        else:
+                            if pos_1 - frag_prop[index_frag_1-1][1] < 500:
+                                dist_1a = pos_1 - frag_prop[index_frag_1-1][2] 
+                                dist_1b = pos_1 - frag_prop[index_frag_1-1][1]
+                                weight_1a = distribution_nice_fragments[dist_1a]
+                                weight_1b = distribution_nice_fragments[dist_1b]
+                            else:
+                                weight_1a = 1
+                                weight_1b = 0
+                        if dir_2 == "+":
+                            if frag_prop[index_frag_2+1][2] - pos_2 < 500:
+                                dist_2a = frag_prop[index_frag_2+1][1] - pos_2
+                                dist_2b = frag_prop[index_frag_2+1][2] - pos_2
+                                weight_2a = distribution_nice_fragments[dist_2a]
+                                weight_2b = distribution_nice_fragments[dist_2b]
+                            else:
+                                weight_2a = 1
+                                weight_2b = 0
+                        else:
+                            if pos_2 - frag_prop[index_frag_2-1][1] < 500:
+                                dist_2a = pos_2 - frag_prop[index_frag_2-1][2] 
+                                dist_2b = pos_2 - frag_prop[index_frag_2-1][1]
+                                weight_2a = distribution_nice_fragments[dist_2a]
+                                weight_2b = distribution_nice_fragments[dist_2b]
+                            else:
+                                weight_2a = 1
+                                weight_2b = 0
+                    except IndexError:
+                        #print(dist_1a,dist_1b,dist_2a,dist_2b)
+                        #seem to just contain negative numbers, probably coming from end of chromosomes and doing random things. had 9 occurencies in test dataset
+                        continue
 
-                data.append(1)
-                data.append(1)
-                row.append(index_frag_1)
-                col.append(index_frag_2)
-                row.append(index_frag_2)
-                col.append(index_frag_1)
+                    #normalize the two weights
+                    sum1 = weight_1a + weight_1b
+                    weight_1a = weight_1a/sum1
+                    weight_1b = weight_1b/sum1
+                    sum2 = weight_2a + weight_2b
+                    weight_2a = weight_2a/sum2
+                    weight_2b = weight_2b/sum2
+                    data.extend((weight_1a*weight_2a,weight_1a*weight_2b,weight_1b*weight_2a,weight_1b*weight_2b))
+                    data.extend((weight_1a*weight_2a,weight_1a*weight_2b,weight_1b*weight_2a,weight_1b*weight_2b))
+
+                    if dir_1 == "+" and dir_2 == "+":
+                        row.append(index_frag_1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1)
+                        col.append(index_frag_2+1)
+                        row.append(index_frag_1+1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1+1)
+                        col.append(index_frag_2+1)
+
+                        col.append(index_frag_1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1)
+                        row.append(index_frag_2+1)
+                        col.append(index_frag_1+1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1+1)
+                        row.append(index_frag_2+1)
+                    elif dir_1 == "+" and dir_2 == "-":
+                        row.append(index_frag_1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1)
+                        col.append(index_frag_2-1)
+                        row.append(index_frag_1+1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1+1)
+                        col.append(index_frag_2-1)
+
+                        col.append(index_frag_1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1)
+                        row.append(index_frag_2-1)
+                        col.append(index_frag_1+1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1+1)
+                        row.append(index_frag_2-1)
+                    elif  dir_1 == "-" and dir_2 == "+":
+                        row.append(index_frag_1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1)
+                        col.append(index_frag_2+1)
+                        row.append(index_frag_1-1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1-1)
+                        col.append(index_frag_2+1)
+
+                        col.append(index_frag_1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1)
+                        row.append(index_frag_2+1)
+                        col.append(index_frag_1-1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1-1)
+                        row.append(index_frag_2+1)
+                    elif dir_1 == "-" and dir_2 == "-":
+                        row.append(index_frag_1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1)
+                        col.append(index_frag_2-1)
+                        row.append(index_frag_1-1)
+                        col.append(index_frag_2)
+                        row.append(index_frag_1-1)
+                        col.append(index_frag_2-1)
+
+                        col.append(index_frag_1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1)
+                        row.append(index_frag_2-1)
+                        col.append(index_frag_1-1)
+                        row.append(index_frag_2)
+                        col.append(index_frag_1-1)
+                        row.append(index_frag_2-1)
+                    else:
+                        raise Exception("error here")
+                else:
+                    if dangling:
+                        if dir_1 == "-":
+                            index_frag_1 += 1
+                        if dir_2 == "-":
+                            index_frag_2 += 1
+                    else:
+                        if dir_1 == "+":
+                            index_frag_1 += 1
+                        if dir_2 == "+":
+                            index_frag_2 += 1
+
+                    data.append(1)
+                    data.append(1)
+                    row.append(index_frag_1)
+                    col.append(index_frag_2)
+                    row.append(index_frag_2)
+                    col.append(index_frag_1)
 
     return data, row, col
 
 def test_distancefromsite(a_file,offsets,valid_chroms,frag_prop,frag_index):
     """tester function to see how these things look like"""
-    #probably 1 milion reads are enough. need to put a limit on that
-    counter_shifts=0
     distribution = [0] * 1000
     distribution_all = [0] * 1000
-    num_reads = 0
     with open(a_file, "r") as pairs:
         for line in pairs:
-            while num_reads < 1000000:
-                info = line.split()
-                frag_1 = info[8]
-                frag_2 = info[9]
-                chr_1 = info[1]
-                chr_2 = info[4]
-                pos_1 = int(info[2])
-                pos_2 = int(info[5])
-                dir_1 = info[3]
-                dir_2 = info[6]
-                if (chr_1 in valid_chroms) and (chr_2 in valid_chroms):
-                    index_frag_1 = frag_index[frag_1]
-                    index_frag_2 = frag_index[frag_2]
-                    try:
-                        if dir_1 == "+":
-                            if pos_1 < frag_prop[index_frag_1][1]:
-                                index_frag_1 -= 1
-                                counter_shifts += 1
-                            distance = frag_prop[index_frag_1][2] - pos_1
-                        else:
-                            if pos_1 > frag_prop[index_frag_1][2]:
-                                index_frag_1 += 1
-                                counter_shifts += 1
-                            distance = pos_1 - frag_prop[index_frag_1][1] 
-                        if frag_prop[index_frag_1-1][3] > 400 and frag_prop[index_frag_1+1][3] > 400 and frag_prop[index_frag_1][3] > 400:
-                            distribution[distance] += 1
-                            num_reads += 1
-                        distribution_all[distance] += 1
-                    except:
-                        continue
+            info = line.split()
+            frag_1 = info[8]
+            frag_2 = info[9]
+            chr_1 = info[1]
+            chr_2 = info[4]
+            pos_1 = int(info[2])
+            pos_2 = int(info[5])
+            dir_1 = info[3]
+            dir_2 = info[6]
+            if (chr_1 in valid_chroms) and (chr_2 in valid_chroms):
+                index_frag_1 = frag_index[frag_1]
+                index_frag_2 = frag_index[frag_2]
+                try:
+                    if dir_1 == "+":
+                        if pos_1 < frag_prop[index_frag_1][1]:
+                            index_frag_1 -= 1
+                        distance = frag_prop[index_frag_1][2] - pos_1
+                    else:
+                        if pos_1 > frag_prop[index_frag_1][2]:
+                            index_frag_1 += 1
+                        distance = pos_1 - frag_prop[index_frag_1][1] 
+                    if frag_prop[index_frag_1-1][3] > 400 and frag_prop[index_frag_1+1][3] > 400 and frag_prop[index_frag_1][3] > 400:
+                        distribution[distance] += 1
+                    # distribution_all[distance] += 1
+                except:
+                    continue
     
     #multiply = sum(distribution_all)/sum(distribution)
     #distribution =  [x * multiply for x in distribution]
