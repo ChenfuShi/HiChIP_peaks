dd1cce43b54b5eabc55ea5555eb7d5bc84ea7e62
ChenfuShi <40122452+ChenfuShi@users.noreply.github.com>
2019-02-20 13:43:13 +0000

    a

--- tests.py
+++ tests.py
@@ -8,15 +8,15 @@ import pickle
 # restriction fragment definitions
 # sizes of chromosomes just to get the right chromosomes
 
-folder = os.path.abspath("/mnt/iusers01/jw01/mdefscs4/psa_functional_genomics/OurData_Hichip/data/homebrew-HiC-Pro/hic_results/data/HaCat_27ac_st")
+folder = os.path.abspath("/mnt/iusers01/jw01/mdefscs4/psa_functional_genomics/Mumbach_other_cells/data/mumbachother-HiC-Pro/hic_results/data/GM12878_H3K27ac")
 resfrag = os.path.abspath("./../domain_caller/testdata/MboI_resfrag_hg38.bed")
 sizes = os.path.abspath("./annotations/hg38.txt")
-temporary_loc = os.path.abspath("./testdata/combined_Mumbach")
+temporary_loc = os.path.abspath("./testdata/GM12878/")
 
 CSR_mat,frag_index,frag_prop,frag_amount,valid_chroms,chroms_offsets = HiCpro_to_sparse(folder,resfrag,sizes,temporary_loc)
 smoothed_diagonal, refined_peaks = sparse_to_peaks(CSR_mat,frag_index,frag_prop,frag_amount,valid_chroms,chroms_offsets,temporary_loc,FDR=0.10)
 
-scipy.sparse.save_npz('./testdata/sparse_matrix_HACAT_st.npz', CSR_mat)
+scipy.sparse.save_npz('./testdata/sparse_matrix_GM12878.npz', CSR_mat)
 
 # with open("./testdata/variables.pi","wb") as picklefile:
 #    pickle.dump([frag_index,frag_prop,frag_amount,valid_chroms,chroms_offsets],picklefile)
