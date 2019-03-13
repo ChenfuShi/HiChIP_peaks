e3316fa35cfa348082c6b7962f54c03cbb06d2d2
ChenfuShi <40122452+ChenfuShi@users.noreply.github.com>
2019-03-13 15:19:26 +0000

    stupid mistake

--- hichip_tool/main.py
+++ hichip_tool/main.py
@@ -107,7 +107,7 @@ def main():
     #if add an option to keep the data for the differential peak calling. then extra script that actually prepares the data for differential peak calling and goes into R
     #would still require the person to manually set design experiments and stuff.
     import pickle
-    with open(os.path.join(output_dir,prefix + "alldata.pickle","wb")) as picklefile:
+    with open(os.path.join(output_dir,prefix + "alldata.pickle"),"wb") as picklefile:
         pickle.dump([CSR_mat,frag_index,frag_prop,frag_amount,valid_chroms,chroms_offsets,smoothed_diagonal, refined_peaks ,quick_peaks, peak_p_vals , peaks_q_vals],picklefile)
 
 
