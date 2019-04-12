6786fe9e39b22369b461945fe12ab2ab556ca411
ChenfuShi <40122452+ChenfuShi@users.noreply.github.com>
2019-04-12 10:25:21 +0100

    removed chromosome X from list of chromosomes

--- hichip_tool/interaction_to_sparse.py
+++ hichip_tool/interaction_to_sparse.py
@@ -141,7 +141,7 @@ def Read_resfrag(resfrag,sizes):
     if sizes==None:
         valid_chroms=['chr1', 'chr2', 'chr3', 'chr4', 'chr5',
                     'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15',
-                    'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX']
+                    'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22']
     else:
         with open(sizes,"r") as file_sizes:
             valid_chroms=[]
